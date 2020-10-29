be92dd63ca46a21c90f0e5e386b82d0d9e8d6cb9
Nhon Dang <dang.nhon12345@yahoo.com>
2020-10-29 23:39:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
 final_results = data.each_with_object({}) do |(key, value), final_array|
   binding.pry
     value.each do |inner_key, names|
-      
+
       names.each do |name|
         if !final_array[name]
           final_array[name] = {}
